5f0a16e65a9afee04a38ad24d6727ef2f80949c3
kalpantika99-DL <kalpantika99@gmail.com>
2025-08-13 13:40:41 +0530

    Add files via upload

---
+++ mathop.py
@@ -0,0 +1,41 @@
+first_value = float(input("enter the first number"))
+second_value = float(input("enter the second number"))
+sum_result = first_value + second_value
+difference_result = first_value - second_value
+product_result = first_value * second_value
+if second_value != 0:
+    division_result = first_value / second_value
+else:
+    division_result = "error! cannot divide by zero."
+print("addition:",sum_result)
+print("subtraction:", difference_result)
+print("Multiplication:", product_result)
+print("division:", division_result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
